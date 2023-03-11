faa76aeaea66f75527a67f2c824fcb9ea5f5b465
mrc <1552397900@qq.com>
2023-03-11 22:37:24 +0800

    2023.03.11.22.37.21

---
+++ src/uis/demo011/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <QApplication>
+#include <QDir>
+#include "ConverQimageOrCvMat.h"
+#include <opencv2/opencv.hpp>
+#include <sstream>
+
+void showImg( const QString &name, const cv::Mat &mulImg ) {
+	std::string winname = name.toLocal8Bit().toStdString();
+	cv::namedWindow(winname, cv::WINDOW_NORMAL);
+	cv::imshow(winname, mulImg);
+}
+
+int main( int argc, char *argv[] ) {
+	QGuiApplication app(argc, argv);
+	auto imgName1 = "./1.png";
+	// 原始图片
+	cv::Mat img1 = cv::imread(QString(imgName1).toLocal8Bit().toStdString(), cv::IMREAD_UNCHANGED);
+	if( img1.empty() )
+		return -1;
+	showImg("input_image", img1);
+
+	int height = img1.rows;
+	int width = img1.cols;
+	double beta = 100,
+		alpha = 0.2;
+	cv::Mat des = cv::Mat(cv::Size(width, height), img1.type());
+	cv::Mat conver;
+	int channels = img1.channels();
+	if( channels == 3 ) {
+		img1.convertTo(conver, CV_64FC3);
+	} else if( channels == 4 ) {
+		img1.convertTo(conver, CV_64FC4);
+	}
+	for( int row = 0; row < height; ++row ) {
+		for( int col = 0; col < width; ++col ) {
+			if( channels == 3 ) {
+				auto &srcVec = conver.at<cv::Vec3d>(row, col);
+				auto &desVec = des.at<cv::Vec3b>(row, col);
+				double b = srcVec[0], g = srcVec[1], r = srcVec[2];
+
+				desVec[0] = cv::saturate_cast<uchar>(b * alpha + beta);
+				desVec[1] = cv::saturate_cast<uchar>(g * alpha + beta);
+				desVec[2] = cv::saturate_cast<uchar>(r * alpha + beta);
+			} else if( channels == 4 ) {
+				auto &srcVec = conver.at<cv::Vec4d>(row, col);
+				double b = srcVec[0], g = srcVec[1], r = srcVec[2];
+				auto &desVec = des.at<cv::Vec4b>(row, col);
+				desVec[0] = cv::saturate_cast<uchar>(b * alpha + beta);
+				desVec[1] = cv::saturate_cast<uchar>(g * alpha + beta);
+				desVec[2] = cv::saturate_cast<uchar>(r * alpha + beta);
+			} else if( channels == 1 ) {
+				des.at<uchar>(row, col) = cv::saturate_cast<uchar>(img1.at<uchar>(row, col) * alpha + beta);
+			}
+		}
+	}
+
+	showImg("out_image", des);
+
+	cv::waitKey();
+	cv::destroyAllWindows();
+	return 0;
+}
---
+++ src/uis/demo012/main.cpp
@@ -0,0 +1,73 @@
+﻿#include <QApplication>
+#include <QDir>
+#include "ConverQimageOrCvMat.h"
+#include <opencv2/opencv.hpp>
+#include <sstream>
+void showImg( const QString &name, const cv::Mat &mulImg );
+void myLine( cv::Mat &img );
+void myRectangle( cv::Mat &img );
+void myEllipse( cv::Mat &img );
+
+int main( int argc, char *argv[] ) {
+	QGuiApplication app(argc, argv);
+	auto imgName1 = "./1.png";
+	// 原始图片
+	cv::Mat img1 = cv::imread(QString(imgName1).toLocal8Bit().toStdString(), cv::IMREAD_UNCHANGED);
+	if( img1.empty() )
+		return -1;
+	showImg("input_image", img1);
+	myLine(img1);
+	showImg("muLin1", img1);
+	myRectangle(img1);
+	showImg("myRectangle", img1);
+	myEllipse(img1);
+	showImg("myEllipse", img1);
+	cv::waitKey();
+	cv::destroyAllWindows();
+	return 0;
+}
+
+void showImg( const QString &name, const cv::Mat &mulImg ) {
+	std::string winname = name.toLocal8Bit().toStdString();
+	cv::namedWindow(winname, cv::WINDOW_NORMAL);
+	cv::imshow(winname, mulImg);
+}
+
+void myLine( cv::Mat &img ) {
+	cv::Point p1 = cv::Point(20, 30);
+	cv::Point p2;
+	p2.x = 300;
+	p2.y = 300;
+	cv::Scalar color = cv::Scalar(0, 0, 255);
+	cv::line(img, p1, p2, color, 1, cv::LINE_8);
+}
+
+void myRectangle( cv::Mat &img ) {
+	cv::Rect rect = cv::Rect(50, 50, 50, 50);
+	cv::Scalar color(255, 0, 0);
+	cv::rectangle(img, rect, color, 2, cv::LINE_4);
+}
+
+void myEllipse( cv::Mat &img ) {
+	cv::ellipse(
+		// 绘制目标
+		img,
+		// 中心坐标
+		cv::Point(img.cols / 2, img.rows / 2),
+		// 给与矩形当中
+		// 水平(img.cols / 4)长度
+		// 垂直(img.rows / 8)长度
+		cv::Size(img.cols / 2, img.rows / 8),
+		// 角度
+		0,
+		// 开始绘制的角度
+		0,
+		// 结束绘制的角度
+		360,
+		// 绘制颜色
+		cv::Scalar(0, 255, 0),
+		// 绘制宽度
+		2,
+		// 线条类型
+		cv::LINE_AA);
+}
